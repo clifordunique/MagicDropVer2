c8b02ff139fcea71264183d5efc5dbc4cd8f8682
kbw-k-tanida <kouji.tanida@knockbackworks.com>
2020-11-30 11:13:24 +0900

    fix ui

---
+++ Assets/02_Scripts/Common/SafeAreaCanvas.cs
@@ -0,0 +1,79 @@
+﻿using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Common.iOS
+{
+    public static class SafeArea
+    {
+#if UNITY_EDITOR
+        private static Rect EditorRect = new Rect(0, 102, 1125, 2202);
+#endif
+        public static Rect Rect =>
+#if UNITY_EDITOR
+            Screen.width == 1125 && Screen.height == 2436 ? EditorRect : Rect.zero;
+#else
+            Screen.safeArea;
+#endif
+    }
+    
+    [RequireComponent(typeof(Canvas))]
+    public class SafeAreaCanvas : MonoBehaviour
+    {
+        [SerializeField] private Color _safeAreaColor = Color.grey;
+        private void Start ()
+        {
+            ApplySafeArea();
+        }
+
+        private void ApplySafeArea()
+        {
+            if (!(0 < SafeArea.Rect.size.y)) return;
+            
+            var areaRect = SafeArea.Rect;
+            var display = UnityEngine.Display.displays[0];
+            var screenSize = new Vector2Int(display.systemWidth, display.systemHeight);
+
+            var anchorMin = areaRect.position;
+            var anchorMax = areaRect.position + areaRect.size;
+            anchorMin.x = 0;
+            anchorMin.y /= screenSize.y;
+            anchorMax.x = 1;
+            anchorMax.y /= screenSize.y;
+            
+            var baseTransform = new GameObject("BaseTransfrom").AddComponent<RectTransform>();
+            baseTransform.SetParent(transform);
+            baseTransform.localScale = Vector3.one;
+            baseTransform.anchorMin = Vector2.zero;
+            baseTransform.anchorMax = Vector2.one;
+            baseTransform.anchoredPosition = Vector3.zero;
+            baseTransform.sizeDelta = Vector2.zero;
+
+            var topSafeAreaTransform = Instantiate(baseTransform, transform).GetComponent<RectTransform>();
+            var contentsTransform = Instantiate(baseTransform, transform).GetComponent<RectTransform>();
+            var bottomSafeAreaTransform = Instantiate(baseTransform, transform).GetComponent<RectTransform>();
+            Destroy(baseTransform.gameObject);
+
+            topSafeAreaTransform.name = "TopSafeArea";
+            contentsTransform.name = "SafeAreaInnerContents";
+            bottomSafeAreaTransform.name = "BottomSafeArea";
+            
+            Enumerable.Range(0, transform.childCount)
+                .Select(i => transform.GetChild(i))
+                .Where(t => !t.Equals(topSafeAreaTransform) && !t.Equals(bottomSafeAreaTransform))
+                .ToList().ForEach(t => t.SetParent(contentsTransform));
+            
+            contentsTransform.anchorMin = anchorMin;
+            contentsTransform.anchorMax = anchorMax;
+
+            topSafeAreaTransform.anchorMin = new Vector2(0, 0);
+            topSafeAreaTransform.anchorMax = new Vector2(1, anchorMin.y);
+            
+            bottomSafeAreaTransform.anchorMin = new Vector2(0, anchorMax.y);
+            bottomSafeAreaTransform.anchorMax = new Vector2(1, 1);
+
+            topSafeAreaTransform.gameObject.AddComponent<RawImage>().color = _safeAreaColor;
+            bottomSafeAreaTransform.gameObject.AddComponent<RawImage>().color = _safeAreaColor;
+        }
+    }
+}
---
+++ Assets/02_Scripts/GameScene/GameOverLogic.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverLogic : MonoBehaviour
+{
+    private const string TargetTag = "Tama";
+    private const int ColumnCount = 7;
+    private const int RowCount = 10;
+
+    [SerializeField] private GameLogic _gameLogic;
+    [SerializeField] private GameUIManager _uiManager;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        if (_gameLogic == null)
+        {
+            _gameLogic = FindObjectOfType<GameLogic>();
+        }
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        
+    }
+
+    //GameOver Check
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag(TargetTag))
+        {
+            StartCoroutine(GameOverCheck());
+        }
+    }
+
+    //GameOver Check Coroutine
+    IEnumerator GameOverCheck()
+    {
+        yield return new WaitForSeconds(0.5f);
+        for (int i = 0; i < ColumnCount; i++)
+        {
+            var isNotGameOver = false;
+            for (int j = 0; j <= RowCount; j++)
+            {
+                var checkIndex = j * ColumnCount;
+                if (_gameLogic.TamaNumList[checkIndex] == RowCount)
+                {
+                    isNotGameOver = true;
+                    break;
+                }
+            }
+            if (isNotGameOver != true)
+            {
+                GameOver();
+                break;
+            }
+        }
+    }
+
+    //GameOver
+    void GameOver()
+    {
+        _uiManager.PopupGameOverOpen();
+    }
+}

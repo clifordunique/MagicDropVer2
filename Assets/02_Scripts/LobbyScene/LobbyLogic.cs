e22c439d2305b930ee00b6f7cb704a5bb3355e97
gbm0426 <geekni@naver.com>
2020-12-09 17:46:19 +0900

    ドロップを離す際の当たり判定の改善 & Lobby UI

---
+++ Assets/02_Scripts/LobbyScene/LobbyLogic.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LobbyLogic : MonoBehaviour
+{
+    //Game Mode
+    public bool clearRule;     //true=Horizontal&Vertical    false=Tetris
+    public bool clearTiming;   //true=Always    false=Drop->Claer
+    public bool tamaCreate;    //true=Top       false=Bottom
+
+
+
+
+    void Awake()
+    {
+        ResetAll();
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    //ResetAll               (void Awake)
+    void ResetAll()
+    {
+        //Game Mode
+        clearRule = true;
+        clearTiming = true;
+        tamaCreate = true;
+    }
+
+    //Btn GameMode ClearRule
+    public void BtnClearRule()
+    {
+        if(clearRule == true)
+        {
+            clearRule = false;
+        }
+        else if(clearRule == false)
+        {
+            clearRule = true;
+        }
+    }
+
+    //Btn GameMode ClearTiming
+    public void BtnClearTiming()
+    {
+        if (clearTiming == true)
+        {
+            clearTiming = false;
+        }
+        else if (clearTiming == false)
+        {
+            clearTiming = true;
+        }
+    }
+
+    //Btn GameMode TamaCreate
+    public void BtnTamaCreate()
+    {
+        if (tamaCreate == true)
+        {
+            tamaCreate = false;
+        }
+        else if (tamaCreate == false)
+        {
+            tamaCreate = true;
+        }
+    }
+}
---
+++ Assets/02_Scripts/LobbyScene/LobbyUIManager.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LobbyUIManager : MonoBehaviour
+{
+    //Popup
+    public GameObject popupGameMode;
+
+    //GameMode
+    bool _clearRule;
+    bool _clearTiming;
+    bool _tamaCreate;
+    public Button btnclearRuleHoriVer;
+    public Button btnclearRuleTetris;
+    public Button btnclearTimingAlways;
+    public Button btnclearTimingAfterDrop;
+    public Button btntamaCreateTop;
+    public Button btntamaCreateBottom;
+
+    [SerializeField] private LobbyLogic _lobbyLogic;
+
+
+
+
+    void Awake()
+    {
+        ResetAll();
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        GameModeButtonCheck();
+    }
+
+    //ResetAll        (void Awake)
+    void ResetAll()
+    {
+        popupGameMode.gameObject.SetActive(false);
+    }
+
+    //GameMode Button Check       (void Update)
+    void GameModeButtonCheck()
+    {
+        _clearRule = _lobbyLogic.clearRule;
+        _clearTiming = _lobbyLogic.clearTiming;
+        _tamaCreate = _lobbyLogic.tamaCreate;
+
+        switch (_clearRule)
+        {
+            case true:
+                btnclearRuleHoriVer.GetComponent<Image>().color = new Color(255 / 255f, 200 / 255f, 0 / 255f);
+                btnclearRuleTetris.GetComponent<Image>().color = new Color(50 / 255f, 50 / 255f, 0 / 255f);
+                break;
+            case false:
+                btnclearRuleHoriVer.GetComponent<Image>().color = new Color(50 / 255f, 50 / 255f, 0 / 255f);
+                btnclearRuleTetris.GetComponent<Image>().color = new Color(255 / 255f, 200 / 255f, 0 / 255f);
+                break;
+        }
+
+        switch (_clearTiming)
+        {
+            case true:
+                btnclearTimingAlways.GetComponent<Image>().color = new Color(255 / 255f, 200 / 255f, 0 / 255f);
+                btnclearTimingAfterDrop.GetComponent<Image>().color = new Color(50 / 255f, 50 / 255f, 0 / 255f);
+                break;
+            case false:
+                btnclearTimingAlways.GetComponent<Image>().color = new Color(50 / 255f, 50 / 255f, 0 / 255f);
+                btnclearTimingAfterDrop.GetComponent<Image>().color = new Color(255 / 255f, 200 / 255f, 0 / 255f);
+                break;
+        }
+
+        switch (_tamaCreate)
+        {
+            case true:
+                btntamaCreateTop.GetComponent<Image>().color = new Color(255 / 255f, 200 / 255f, 0 / 255f);
+                btntamaCreateBottom.GetComponent<Image>().color = new Color(50 / 255f, 50 / 255f, 0 / 255f);
+                break;
+            case false:
+                btntamaCreateTop.GetComponent<Image>().color = new Color(50 / 255f, 50 / 255f, 0 / 255f);
+                btntamaCreateBottom.GetComponent<Image>().color = new Color(255 / 255f, 200 / 255f, 0 / 255f);
+                break;
+        }
+    }
+
+    //Popup GameMode Open
+    public void BtnPopupGameModeOpen()
+    {
+        popupGameMode.gameObject.SetActive(true);
+    }
+
+    //Popup GameMode Close
+    public void BtnPopupGameModeClose()
+    {
+        popupGameMode.gameObject.SetActive(false);
+    }
+}
